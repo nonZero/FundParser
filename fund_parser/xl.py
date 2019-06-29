15dbbc21e61ff04ca927f76f0c81344dcf278a95
Udi Oron <udioron@gmail.com>
2019-06-29 23:07:16 +0300

    Skip hidden sheets

--- fund_parser/xl.py
+++ fund_parser/xl.py
@@ -8,4 +8,4 @@ def extract_cells(sh):
 def extract_sheets(fn: str):
     b = xlrd.open_workbook(fn)
     it = zip(b.sheet_names(), b.sheets())
-    return {n: extract_cells(sh) for n, sh in it}
+    return {n: extract_cells(sh) for n, sh in it if sh.visibility == 0}
