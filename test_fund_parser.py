215c5b2503310fc08410c23e8099469152166430
Udi Oron <udioron@gmail.com>
2019-06-30 11:32:54 +0300

    Added test_data as submodule

--- test_fund_parser.py
+++ test_fund_parser.py
@@ -1,20 +1,34 @@
+import csv
+from collections import Counter
 from pathlib import Path
 
-import pandas as pd
-import pytest
+import numpy as np
 
 from fund_parser.extract import extract_fund_data
 
 
-@pytest.fixture
-def data_path():
-    return Path(__file__).parent / "test_data"
+def collect_tests():
+    data_path = Path(__file__).parent / "test_data"
+    with (data_path / "expected.csv").open() as f:
+        reader = csv.DictReader(f)
+        for d in reader:
+            yield (
+                str(data_path / d['filename']),
+                int(d['sheet_count']),
+                int(d['none_count']),
+            )
 
 
-def test_extract(data_path):
-    filename = data_path / "520004078_b259010_p418.xlsx"
-    general, sheets = extract_fund_data(filename)
-    assert isinstance(general, list)
-    assert len(sheets) == 29
-    for k, v in sheets.items():
-        assert v is None or isinstance(v, pd.DataFrame)
+def pytest_generate_tests(metafunc):
+    tests = list(collect_tests())
+
+    metafunc.parametrize(
+        "xls_filename,expected_sheets,expected_empty_sheets", tests)
+
+
+def test_xls(xls_filename, expected_sheets, expected_empty_sheets):
+    general, sheets = extract_fund_data(xls_filename)
+    assert isinstance(general, np.ndarray)
+    c = Counter(type(v).__name__ for v in sheets.values())
+    assert c['DataFrame'] == expected_sheets
+    assert c['NoneType'] == expected_empty_sheets
